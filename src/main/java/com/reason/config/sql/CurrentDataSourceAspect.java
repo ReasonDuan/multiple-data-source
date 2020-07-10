eba0932165278a034c2d0f6e42a87fbc20b34029
rd87 <reason.h.duan@newegg.com>
2020-07-10 16:00:10 +0800

    add remark

--- src/main/java/com/reason/config/sql/CurrentDataSourceAspect.java
+++ src/main/java/com/reason/config/sql/CurrentDataSourceAspect.java
@@ -7,7 +7,7 @@ import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
 
 /**
- * @Description:
+ * @Description: 切面， 更改数据源
  * @Author: Reason.H.Duan
  * @Date: 7/10/2020
  */
@@ -15,12 +15,22 @@ import org.springframework.stereotype.Component;
 @Component
 public class CurrentDataSourceAspect {
 
+    /**
+     * 在CurrentDataSource执行前切换数据源
+     * @param point 切面对象
+     * @param currentDataSource 数据源切换注解对象
+     */
     @Before("@annotation(currentDataSource)")
     public void beforeSwitchDB(JoinPoint point, CurrentDataSource currentDataSource){
         String dbName = currentDataSource.value();
         DynamicDataSource.setDB(dbName);
     }
 
+    /**
+     * 在CurrentDataSource执行后清湖当前数据源
+     * @param point 切面对象
+     * @param currentDataSource 数据源切换注解对象
+     */
     @After("@annotation(currentDataSource)")
     public void afterSWitchDB(JoinPoint point, CurrentDataSource currentDataSource){
         DynamicDataSource.clearDB();
